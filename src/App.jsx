4af8383037f12d894c65787a77150e49b71d5a7c
Shwetalbhargav <hridaan.purav@gmail.com>
2025-08-07 19:21:08 +0530

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate
-} from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar      from './components/Sidebar';
 import ChatList     from './components/ChatList';
 import ChatWindow   from './components/ChatWindow';
@@ -29,7 +23,7 @@ function App() {
         <main className="flex-fill">
           <Routes>
             {/* redirect root to /chat */}
-            <Route path="/" element={<Navigate to="/chat" replace />} />
+            <Route path="/" element={<Navigate replace to="/chat"  />} />
 
             {/* you can later render a “blank” view at /chat if no waId */}
             <Route path="/chat" element={<div className="p-4">Select a chat</div>} />
