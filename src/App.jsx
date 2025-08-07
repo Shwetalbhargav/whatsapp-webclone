95b4cb30d2b9997915e93ff50cf17a0cf82c85ad
Shwetalbhargav <hridaan.purav@gmail.com>
2025-08-07 18:20:37 +0530

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,30 @@ import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <Router>                            {/* ← opening tag */}
+    <Router>
       <div className="d-flex vh-100">
-        <aside className="col-4 border-end">
-          <Sidebar/>
+        {/* 1: Icon sidebar – fixed width */}
+        <aside style={{ width: 60 }} className="border-end">
+          <Sidebar />
+        </aside>
+
+        {/* 2: Chat list – fixed width or responsive */}
+        <aside style={{ width: 300 }} className="border-end">
           <ChatList />
         </aside>
 
+        {/* 3: Chat window – takes all remaining space */}
         <main className="flex-fill">
           <Routes>
+            {/* Optional redirect from root */}
+            <Route path="/" element={<Navigate to="/chat" replace />} />
+
+            {/* Your actual chat route */}
             <Route path="/chat/:waId" element={<ChatWindow />} />
           </Routes>
         </main>
       </div>
-    </Router>                           
+    </Router>                        
   );
 }
 
